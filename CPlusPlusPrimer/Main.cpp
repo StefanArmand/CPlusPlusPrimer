385cd5f6e4502edc1a443815937cde40e3c66798
Stefan <stefan_armand@yahoo.com>
2018-06-09 11:00:54 +0300

    Ch4Done

--- CPlusPlusPrimer/Main.cpp
+++ CPlusPlusPrimer/Main.cpp
@@ -1,8 +1,75 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	//int n = 24;
+	//if (n % 2 == 0) {
+	//	cout << "n is even" << endl;
+	//}
+	//else {
+	//	cout << "n is odd" << endl;
+	//}
 
+
+	//exercise 4.10
+	//int val = 0;
+	//while (cin >> val && val != 42) {
+	//	cout << val << endl;
+	//}
+
+	//exercise 4.11
+	/*if(a>b && b > c && c > d)*/
+
+	//exercise 4.21
+	/*vector<int> values{ 3, 5, 6, 7, 8, 9 };
+	for (int i = 0; i < values.size(); i++) {
+		int results = ((values[i] % 2) > 0) ? values[i] : 0;
+		if (results == 0) {
+			cout << "";
+		}
+		else {
+			cout << results * 2 << endl;
+		}
+	}*/
+
+	//exercise 4.22
+	//int grade = 25;
+	////string finalgrade = (grade > 90) ? "high pass" : (grade <= 75) ? "low pass" : (grade < 60) ? "fail" : "pass";
+	//if (grade > 90) {
+	//	cout << "high pass" << endl;
+	//}
+	//else if (grade <= 75 && grade > 60) {
+	//	cout << "low pass" << endl;
+	//}
+	//else if (grade > 75 && grade < 90) {
+	//	cout << "pass" << endl;
+	//}
+	//else {
+	//	cout << "fail" << endl;
+	//}
+	////cout << finalgrade << endl;
+	//// if statements are easier to understand but longer to write
+
+	//string s = "word";
+	//string pl = (s + 
+	//	s[s.size() - 1]) 
+	//	== "s" ? "" : "s";
+	//cout << pl << endl;
+
+	//exercise 4.28
+	//char c = ' ';
+	//int in = 1;
+	//int *p = &in;
+	/*int x [10] ;
+	cout << sizeof *x << endl;*/
+
+	//int x[10]; int *p = x;
+	//cout << sizeof(x) / sizeof(*x) << endl;
+	//cout << sizeof(p) / sizeof(*p) << endl;
+
+	system("pause");
 }
