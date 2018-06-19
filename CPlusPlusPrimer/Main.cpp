7388f14ec699aa9ff3eb760cb64598fe87de56e2
Stefan <stefan_armand@yahoo.com>
2018-06-19 09:28:07 +0300

    Ch7 ex1-3 done

--- CPlusPlusPrimer/Main.cpp
+++ CPlusPlusPrimer/Main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <string>
+#include "Main.h"
 
 using namespace std;
 
+Sales_data& Sales_data::combine(const Sales_data &rhs) {
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+
 int main()
 {
+	Sales_data total;
+	if (cin >> total.bookNo >> total.units_sold) {
+		Sales_data trans;
+		int totalrev = 0;
+		while (cin >> trans.bookNo >> trans.units_sold) {
+			if (total.bookNo == trans.bookNo) {
+				int totnumbsold = (total.revenue * total.units_sold);
+				totnumbsold += trans.revenue * trans.units_sold;
+				totalrev += totnumbsold;
+				total.combine(trans);
+			}
+			else {
+				cout << "Revenue: " << totalrev << endl;
+				cout << "Units sold: "<<total.units_sold << endl;
+				total = trans;
+			}
+		}
+	}
+	else {
+		cerr << "No data?!" << endl;
+		return -1;
+	}
+	cout << total.revenue << endl;
+	
+
+	system("pause");
 
 }
--- CPlusPlusPrimer/Main.h
+++ CPlusPlusPrimer/Main.h
@@ -1,1 +1,13 @@
 #pragma once
+#ifndef MAIN_H
+#define MAIN_H
+#include <string>
+
+struct Sales_data {
+	std::string isbn() const { return bookNo; }
+	Sales_data& combine(const Sales_data&);
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 5.0;
+};
+#endif
