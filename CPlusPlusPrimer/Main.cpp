500064322e95783258868e88a982b9daad28efea
Stefan <stefan_armand@yahoo.com>
2018-06-19 18:07:17 +0300

    Ch 7 Ex 22 done

--- CPlusPlusPrimer/Main.cpp
+++ CPlusPlusPrimer/Main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 istream & read(istream &is, Person &item) {
-	is >> item.name >> item.address >> price;
+	is >> item.name >> item.address;
 	return is;
 }
 
 ostream &print(ostream &os, const Person &item) {
-	os << item.returnname() << " " << item.returnaddress << endl;
+	os << item.returnname() << " " << item.returnaddress() << endl;
 	return os;
 }
 
--- CPlusPlusPrimer/Main.h
+++ CPlusPlusPrimer/Main.h
@@ -4,10 +4,18 @@
 #include <string>
 
 struct Person {
+	Person() = default;
+	Person(const std::string adr) : address(adr) {}
+	Person(const std::string adr, const std::string n) : address(adr), name(n) {}
 	std::string returnname() const { return name; }
 	std::string returnaddress() const { return address; }
+	friend std::istream &read(std::istream &, Person &);
+
+private:
 	std::string name;
 	std::string address;
 };
+
+std::istream & read(std::istream &, Person &);
 
 #endif
