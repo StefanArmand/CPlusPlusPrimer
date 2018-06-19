a1cf0dbaa550423ef16cf328394865d7deb1ac09
Stefan <stefan_armand@yahoo.com>
2018-06-19 15:26:31 +0300

    Ch 7 ex 10 done

--- CPlusPlusPrimer/Main.cpp
+++ CPlusPlusPrimer/Main.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+istream & read(istream &is, Person &item) {
+	is >> item.name >> item.address >> price;
+	return is;
+}
+
+ostream &print(ostream &os, const Person &item) {
+	os << item.returnname() << " " << item.returnaddress << endl;
+	return os;
+}
+
 int main()
 {
 
