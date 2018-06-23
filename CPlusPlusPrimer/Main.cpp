8b2915549db5d8fb612b335029c4f9031d4d6492
Stefan <stefan_armand@yahoo.com>
2018-06-23 15:42:56 +0300

    Ch8 fast pass

--- CPlusPlusPrimer/Main.cpp
+++ CPlusPlusPrimer/Main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+
+// exercise 8.1
+//istream& process_input(istream& is) {
+//	string inpt = " ";
+//	while (is >> inpt) {
+//		cout << inpt << endl;
+//	}
+//	auto old_state = cin.rdstate();
+//	is.clear();
+//	is.setstate(old_state);
+//	return is;
+//}
+
+// exercise 8.3
+vector<string> aVector(string &filename) {
+	ifstream in(filename);
+	vector<string> vs;
+	for (string s; in >> s;) {
+		vs.push_back(s);
+	}
+	return vs;
+}
+
 int main()
 {
+	//process_input(cin);
+
 
 }
