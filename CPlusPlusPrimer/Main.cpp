8ed1b529d29b6c9a00f6a878b06c8bdbc80af32d
Stefan <stefan_armand@yahoo.com>
2018-06-29 13:37:11 +0300

    Ch9 WIP

--- CPlusPlusPrimer/Main.cpp
+++ CPlusPlusPrimer/Main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <vector>
+#include "Main.h"
 
 using namespace std;
 
+//bool findvalue(int first, int second, int ValueToFind) {
+//	vector<int> v;
+//	int val = first;
+//	bool isvalue = false;
+//	while (first < second) {
+//		v.push_back(first);
+//		first++;
+//	}
+//	first = val;
+//	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it){
+//		if (ValueToFind == *it) {
+//			isvalue = true;
+//		}
+//	}
+//	return isvalue;
+//}
+
 int main()
 {
-
+	//if (findvalue(5, 21, 27)) {
+	//	cout << "the value exists" << endl;
+	//}
+	//else {
+	//	cout << "the value doesn't exists" << endl;
+	//}
+	vector<int> v1{ 1 };
+	vector<int> v1 (1);
+	vector<int> v1( 5, 10 );
+	vector<int> v1 = { 1 };
+	system("pause");
 }
